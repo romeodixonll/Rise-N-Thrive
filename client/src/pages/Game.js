882ea164e3f4441993d959720cde4742e16596f9
unknown <zarsement@hotmail.com>
2022-03-11 13:32:15 -0600

    Penguin game is fully integrated into application and functional

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,13 +1,10 @@
 import classes from './Game.module.css'
-import Phaser from 'phaser'
-import PenguinGame from '../components/PageComponents/game/penguinScript'
-import penguinImg from '../assets/images/penguin-images/lil-penguin-idle.png';
-import krillImg from '../assets/images/penguin-images/krill.png';
+import React from 'react'
 
 const Game = () => {
     return (
     <div className={`${classes.flex} game-content`} style={{ height: '94vh'}} id='game-content'>
-        {/* <PenguinGame /> */}
+        <a href="/penguin-game/scene.html">Click Me!</a>
     </div>
 )}
 
--- server/server.js
+++ server/server.js
@@ -14,16 +14,20 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// app.get("/penguin", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/penguin.html"));
+// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
