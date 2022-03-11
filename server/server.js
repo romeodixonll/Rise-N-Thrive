693f13529837f83b95c4ef47d29b0540b88165da
Michael Klein <michaelklein1997@gmail.com>
2022-03-11 13:37:23 -0600

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -14,20 +14,16 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-// app.get("/penguin", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/penguin.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
