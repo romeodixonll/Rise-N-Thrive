21dcb8f25054bcdddaee36e560b8fb0897c6f5be
Romeo Dixon <romeodixon2nd@gmail.com>
2022-03-14 10:07:27 -0500

    updated the login and signup page.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,12 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-  }
+type User {
+  _id: ID
+  firstName: String
+  lastName: String
+  email: String
+}
 
   type Auth {
     token: ID!
@@ -20,7 +20,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
   }
 `;
